d1836c26cad4e885cf30f40fe23ff9c39d34affc
ianjazz246 <ianjazz246@gmail.com>
2021-10-14 20:54:57 -0700

    Smaller networks

--- mediapipe/train.py
+++ mediapipe/train.py
@@ -4,10 +4,12 @@ import keras
 import numpy as np
 import itertools
 import math
+
 from common import GESTURE_CATEGORIES, hand_landmark_to_model_input
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras import optimizers
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
@@ -18,7 +20,7 @@ import ast
 MEDIAPIPE_DIR = "mediapipe"
 NUM_LANDMARKS = 21
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 20
 
 hand_coordinates = []
 y = []
@@ -56,24 +58,29 @@ y = to_categorical(y)
 
 print(X.shape)
 
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
 
 model = Sequential()
 
-model.add(Dense(100, activation="relu", input_shape=(math.comb(NUM_LANDMARKS, 2),)))
-model.add(Dense(25, activation="relu"))
+#model.add(Dense(100, activation="relu", input_shape=(math.comb(NUM_LANDMARKS, 2),)))
+model.add(Dense(50, activation="relu", input_shape=(math.comb(NUM_LANDMARKS, 2),)))
+model.add(Dense(50, activation="relu"))
+model.add(Dense(15, activation="relu"))
+#model.add(Dense(10, activation="relu"))
+
+# Output layer
 model.add(Dense(len(GESTURE_CATEGORIES), activation="softmax"))
 
 print(model.summary())
 
+cce = CategoricalCrossentropy()
+
 optimizer = optimizers.Adam(learning_rate=0.01)
-model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+model.compile(loss=cce, optimizer=optimizer, metrics=["accuracy"])
 
 callbacks = [
     keras.callbacks.ModelCheckpoint(
-        filepath="mediapipe/8_gestures_best.h5",
+        filepath="mediapipe/8_gestures_smaller2_best.h5",
 				save_best_only = True,
 				monitor="val_loss",
 		),
@@ -88,7 +95,11 @@ history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, vali
 
 y_pred = model.predict(X_test, batch_size=BATCH_SIZE)
 
+loss = cce(y_test, y_pred).numpy()
+print("Loss:", loss)
 matrix = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+
+print(history.history['val_loss'])
 
 disp = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=GESTURE_CATEGORIES)
 disp.plot()
